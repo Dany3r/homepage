a81388e6d7f441e2e2058a22a2e414671250e433
Redak0313 <cutedigitaldevelepoer8@gmail.com>
2024-08-04 20:59:52 -0400

    feat: :tada: publish to github

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import  burgerMenu  from "..//assets/images/icon-menu.svg";
+export const NavBar = () => {
+  return (
+    <>
+    <ul className="hidden sm:flex text-[18px] sm:w-[428px] 
+    sm:place-content-around sm:text-[16px] sm:items-center ">
+
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Popular</a>
+        </li>
+        <li>
+            <a href="#">Treading</a>
+        </li>
+        <li>
+            <a href="#">Categorias</a>
+        </li>
+    </ul>
+    <img className="w-20 h-4 cursor-pointer sm:hidden" 
+    src={burgerMenu} alt="Menu Hamburgesa" />
+    </>
+  )
+}
