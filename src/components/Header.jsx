a5916af544aa93dfe0d5a59a5e570c1645fc73c2
Dany3r <danyeracuna9@gmail.com>
2024-08-06 16:40:18 -0400

    header fijo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { NavBar } from "./NavBar";
 import logo from '../assets/images/logo.svg'
 export const Header = () => {
   return (
-    <header className="flex place-content-between items-center mb-8 ... sticky top-0 "> 
+    <header className="flex place-content-between items-center mb-8 ... sticky top-0 bg-white "> 
         <img src={logo} alt='Logo'/>
         <NavBar/>
     </header>
